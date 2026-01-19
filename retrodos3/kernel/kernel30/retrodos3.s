433828a4f7bf8d56d33af144b39e1d589292e199
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-01-20 01:36:19 +0300

    Add files via upload

--- retrodos3/kernel/kernel30/retrodos3.s
+++ retrodos3/kernel/kernel30/retrodos3.s
@@ -1,7 +1,7 @@
 ; ****************************************************************************
 ; RETRODOS.SYS (MSDOS 3.3 Kernel) - RETRO DOS v3.0 by ERDOGAN TAN
 ; ----------------------------------------------------------------------------
-; Last Update: 10/07/2024 (BugFix) - (Previous: 22/01/2024)
+; Last Update: 22/01/2024 (BugFix) - (Previous: 28/08/2023)
 ; ----------------------------------------------------------------------------
 ; Beginning: 24/02/2018 (Retro DOS 2.0), 25/05/2018 (Retro DOS 3.0)
 ; ----------------------------------------------------------------------------
@@ -10709,7 +10709,6 @@ align 16 ; Paragraph alignment is necessary here for MSDOS kernel relocation
 ; 11/06/2018 - Retro DOS v3.0 
 
 MSDOS_BIN_OFFSET:  ; this offset must be paragraph aligned
-		; 10/07/2024 (BugFix)
 		; 22/01/2024 (BugFix)
 		; 16/01/2024 (BugFix)
 		; 22/11/2022 (BugFix)
